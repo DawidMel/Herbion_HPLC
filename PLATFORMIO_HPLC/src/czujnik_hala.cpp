0c90fc3d430386baf3be80ec7b036fef741de2f6
Dawid Meleszczuk <dawid.meleszczuk@gmal.com>
2024-11-08 02:06:59 +0100

    restore sensor arduino code

--- PLATFORMIO_HPLC/src/czujnik_hala.cpp
+++ PLATFORMIO_HPLC/src/czujnik_hala.cpp
@@ -1,1 +1,27 @@
 #include"czujnik_hala.hpp"
+
+/*
+
+// program, który przeniesie pin 10 w stan niski 
+// po wykryciu pola magentycznego i zaświeci diodę na pinie 13
+// https://botland.com.pl/pl/czujniki-magnetyczne/14326-iduino-czujnik-halla-pola-magnetycznego.html
+
+
+int Led = 13 ; // define LED Interface
+int SENSOR = 10 ; // define the Hall magnetic sensor interface
+int val ; // define numeric variables val
+void setup () {
+  pinMode (Led, OUTPUT) ;    // define LED as output interface
+  pinMode (SENSOR, INPUT) ;  // define the Hall magnetic sensor line as input
+}
+
+void loop () {
+  val = digitalRead (SENSOR) ; // read sensor line
+  if (val == LOW) { // when the Hall sensor detects a magnetic field, Arduino LED lights up
+    digitalWrite (Led, HIGH);
+  }
+  digitalWrite (Led, LOW);
+}
+
+
+*/
--- PLATFORMIO_HPLC/src/czujnik_odleglosci.cpp
+++ PLATFORMIO_HPLC/src/czujnik_odleglosci.cpp
@@ -1,1 +1,29 @@
 #include"czujnik_odleglosci.hpp"
+
+/*
+int Obstacles_din=2;
+int Obstacles_ain=A0;
+int ad_value;
+void setup()
+{
+  pinMode(Obstacles_din,INPUT);
+  pinMode(Obstacles_ain,INPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+  ad_value=analogRead(Obstacles_ain);
+  if(digitalRead(Obstacles_din)==LOW)
+  {
+    Serial.println("Near the Obstacles");
+    Serial.println(ad_value);
+  }
+  else
+  {
+    Serial.println("Far the Obstacles");
+  }
+  delay(500);
+}
+
+
+*/
